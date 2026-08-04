61f1a22641fb272ce4c7a2a2650b5a0d59e2640f
keryn.hwang <hyyy8810@gmail.com>
2026-08-04 11:42:47 +0900

    bugFix : 게시글 시간 에러 해결

--- react-home/src/utils/date.js
+++ react-home/src/utils/date.js
@@ -1,31 +1,9 @@
-// 백엔드 UTC 시간을 서울 시간으로 변환
+// 백엔드에서 내려준 서울 시간을 화면 형식으로 표시
 export function formatSeoulDate(date, { withSeconds = false } = {}) {
   if (!date) return "";
 
-  const value = String(date);
-  const hasTimeZone = /(?:Z|[+-]\d{2}:?\d{2})$/i.test(value);
-  const parsedDate = new Date(hasTimeZone ? value : `${value}Z`);
-
-  if (Number.isNaN(parsedDate.getTime())) return value.replace("T", " ");
-
-  const parts = new Intl.DateTimeFormat("en-CA", {
-    timeZone: "Asia/Seoul",
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    ...(withSeconds ? { second: "2-digit" } : {}),
-    hourCycle: "h23",
-  }).formatToParts(parsedDate);
-  const dateParts = Object.fromEntries(
-    parts.map(({ type, value: partValue }) => [type, partValue]),
-  );
-
-  const formattedDate = `${dateParts.year}-${dateParts.month}-${dateParts.day}`;
-  const formattedTime = `${dateParts.hour}:${dateParts.minute}`;
-
-  return withSeconds
-    ? `${formattedDate} ${formattedTime}:${dateParts.second}`
-    : `${formattedDate} ${formattedTime}`;
+  return String(date)
+    .replace(/Z$|[+-]\d{2}:?\d{2}$/i, "")
+    .replace("T", " ")
+    .slice(0, withSeconds ? 19 : 16);
 }
