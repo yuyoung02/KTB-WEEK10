b83884f02895f6ab1db0d6f6ee4720d659484dc6
keryn.hwang <hyyy8810@gmail.com>
2026-08-06 16:50:53 +0900

    CICD: 테스트 부분 제거

--- react-home/src/pages/LoginPage/LoginPage.jsx
+++ react-home/src/pages/LoginPage/LoginPage.jsx
@@ -43,7 +43,7 @@ function LoginPage() {
     <>
       <header className="login-header">
         <Link to="/" className="login-logo">
-          <h1>구장 이야기 CICD 램 확인 ⚾️</h1>
+          <h1>구장 이야기 ⚾️</h1>
         </Link>
       </header>
 
